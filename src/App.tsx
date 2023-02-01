8b03efc9cc7d578ff9ff5a9abe26afbfca659dbe
Saman <saman@foodbomb.com.au>
2023-02-01 21:43:00 +0800

    add fixed angled fullname to the left side of the page

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
-import { useState } from 'react';
 import styles from './App.module.scss';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className={styles.App}>
-      <h1>Hello Saman</h1>
-      <button className={styles.Button} onClick={() => setCount((prev) => prev + 1)}>
-        Count {count}
-      </button>
+      <h1 className={styles.AngledFullName}>Saman Fayazi</h1>
     </div>
   );
 }
